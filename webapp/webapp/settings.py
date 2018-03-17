bf32a2cc8fdd44aba4b004949db82b96369202bb
sevedetta <detta.severino-tit15@it.dhbw-ravensburg.de>
2018-03-17 12:37:03 +0100

    DB Verbindung geändert

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -87,8 +87,13 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'StudienarbeitDB',
+        'USER':'studienarbeit',
+        'PASSWORD':'admin',
+        'HOST':'localhost',
+        # TODO create new DB with origianl protnumber 3306
+        'PORT':'3307',
     }
 }
 
