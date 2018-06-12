2a95cb667e59e686be61b5937cda621753283f55
jonasjarling <jonas.jarling@ewetel.net>
2018-06-12 16:47:11 +0200

    Minor changes footer dsgvo vorbereitet

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 class MyUserManager(BaseUserManager):
     def create_user(self, email,  password=None):
         """
-        Creates and saves a User with the given email, date of
-        birth and password.
+        Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
