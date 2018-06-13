9b4bb8982671888f2730ab3f28e341e33d7fcb7c
jonasjarling <jonas.jarling@ewetel.net>
2018-06-13 23:53:42 +0200

    workput test

--- webapp/training/admin.py
+++ webapp/training/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Exercise, Equipment, Trainingplan
+from .models import Exercise, Equipment, Workout, Routine
 # Register your models here.
 
 admin.site.register(Exercise)
 admin.site.register(Equipment)
-admin.site.register(Trainingplan)
+admin.site.register(Workout)
+admin.site.register(Routine)
