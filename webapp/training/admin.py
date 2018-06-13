9f0ef80f416b953d40c4ec7417080c26a50f9ab5
jonasjarling <jonas.jarling@ewetel.net>
2018-06-13 22:51:21 +0200

    including training Models

--- webapp/training/admin.py
+++ webapp/training/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Exercise, Equipment, Trainingplan
 # Register your models here.
+
+admin.site.register(Exercise)
+admin.site.register(Equipment)
+admin.site.register(Trainingplan)
