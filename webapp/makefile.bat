7ff850d2749c97d1455520d6d83df893138185af
sevedett <detta.severino-tit15@it.dhbw-ravensburg.de>
2018-06-14 21:31:46 +0200

    - hardcoded batch script, because i screwed up writing migration commands ..

---
+++ webapp/makefile.bat
@@ -0,0 +1,20 @@
+echo Start batch script
+
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py makemigrations training
+python manage.py migrate training
+
+python manage.py makemigrations userprofile
+python manage.py migrate userprofile
+
+python manage.py makemigrations main
+python manage.py migrate main
+
+python manage.py makemigrations statistic
+python manage.py migrate statistic
+
+python manage.py runserver
+
+echo End batch script
