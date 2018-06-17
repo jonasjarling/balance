90575057b3c46885c346ecf653e35f6df7189784
sevedett <detta.severino-tit15@it.dhbw-ravensburg.de>
2018-06-17 16:50:03 +0200

    - hardcoded list object of training-card - deleted exercise - bla bla bla

--- webapp/training/models.py
+++ webapp/training/models.py
@@ -5,49 +5,57 @@ from django.contrib.auth.models import User
 
 
 class Equipment(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
     number = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
+    def as_dict(self):
+        return {
+            "name": self.name,
+            "number": self.number
+        }
 
-class Exercise(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
+
+class Routine(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    setup = models.CharField(max_length=200, blank=True, null=True)
+    reps = models.IntegerField(default=0)
+    sets = models.IntegerField(default=0)
     description = models.CharField(max_length=500, blank=True, null=True)
-    equipment = models.ManyToManyField(Equipment, blank=True)
-    picture = models.ImageField(upload_to='training/', height_field=None, width_field=None, max_length=100, blank=True, null=True)
+    equipment = models.ForeignKey(Equipment, blank=True, null=True, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='training/', height_field=None, width_field=None, max_length=100, blank=True,
+                                null=True)
+
+    def as_dict(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "setup": self.setup,
+            "reps": self.reps,
+            "sets": self.sets,
+            "equipment": self.equipment,
+            "picture": self.picture.url
+        }
 
     def __str__(self):
         return self.name
-
-    def exercise_as_dict(self):
-        return {
-            str(self.id): {
-                "name": self.name,
-                "description": self.description,
-                "equipment": self.equipment,
-                "picture": self.picture
-            }
-        }
 
 
 class Workout(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     createdBy = models.CharField(max_length=50, blank=True, null=True)
-    user = models.ManyToManyField(User, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    routine = models.ManyToManyField(Routine, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-
-class Routine(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-    exercise = models.ManyToManyField(Exercise, blank=True)
-    workout = models.ManyToManyField(Workout, blank=True)
-    setup = models.CharField(max_length=200, blank=True, null=True)
-    reps = models.IntegerField(default=0)
-    sets = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
+    def as_dict(self):
+        return {
+            "name": self.name,
+            "createdBy": self.createdBy,
+            "user": self.user,
+            "routine": self.routine.values()
+        }
