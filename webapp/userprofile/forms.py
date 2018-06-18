509289ff0c1029eeafe4f1d614b78c01f77cfe6d
sevedett <detta.severino-tit15@it.dhbw-ravensburg.de>
2018-06-18 17:32:16 +0200

    - included forms to modify models

---
+++ webapp/userprofile/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User, Profile, BodyStats, Weight
+
+class WeightForm(forms.ModelForm):
+    #user = Weight.user
+    #date = Weight.date
+    #weight = Weight.weight
+    class Meta:
+        model = Weight
+        fields = ('weight',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('picture', 'height', 'weight', 'fat', 'muscle', 'bone', 'water',)
+
